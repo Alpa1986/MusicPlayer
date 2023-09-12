1db1fa7c91c148d1da1a001d95af5190aa1380da
Alpa <makaveli1986@hotmail.de>
2023-09-12 15:53:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+const key = "	41fb067e74ee14abc95ad4a3cbc67bda";
 
 function App() {
+  useEffect(() => {
+    const url = `https://api.musixmatch.com/ws/1.1/track.get?apikey=${key}`;
 
+    fetch(url)
+      .then((data) => {
+        return data.json();
+      })
+      .then((result) => {
+        console.log(result);
+      });
+  });
 
-  return (
-    <>
-  <h1>Hi world</h1>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
